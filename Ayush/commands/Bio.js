014f32696a62edfba06073e533eea379f55a5e21
mohitkk5858-bit <mohitkk5858@gmail.com>
2026-03-18 15:45:11 +0530

    Update bot bio command credits and usage

--- Ayush/commands/Bio.js
+++ Ayush/commands/Bio.js
@@ -2,10 +2,10 @@ module.exports.config = {
 	name: "bio",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "Rachit Thakur",
 	description: "Change bot's bio",
 	commandCategory: "admin",
-	usages: "bio [text]",
+	usages: "bio [Don]",
   cooldowns: 5
   
 }
