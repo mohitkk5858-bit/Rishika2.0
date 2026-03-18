ce067d11c43608e7cee86a6e0bedf796f1c79531
mohitkk5858-bit <mohitkk5858@gmail.com>
2026-03-18 19:27:48 +0530

    Update credits and permissions in appstate.js

--- Ayush/commands/appstate.js
+++ Ayush/commands/appstate.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "appstate",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "Rachit Thakur",
   description: "refresh appstate.json",
   commandCategory: "Admin",
   usages: "appstate",
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event, args }) {
   const fs = require("fs-extra");
-  const permission = ["100040426712109"];
+  const permission = ["61577622457037"];
 	if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command", event.threadID, event.messageID);
   let appstate = api.getAppState();
   // convert JSON object to a string
